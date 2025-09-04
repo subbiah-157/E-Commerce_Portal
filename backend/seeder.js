4f011750ee7b98ff731040ac8f0e15aa293b83f3
SUBBIAH MUTHU M <subbiahmuthu397@gmail.com>
2025-09-04 09:03:35 +0530

    Update backend seeder.js

--- backend/seeder.js
+++ backend/seeder.js
@@ -28,20 +28,24 @@ const seedUsers = async () => {
         password: await bcrypt.hash("deli@123", 10),
         role: "delivery",
       },
+      {
+        name: "Support",
+        email: "support@gmail.com",
+        password: await bcrypt.hash("support@123", 10),
+        role: "support",
+      },
     ];
 
     for (const user of users) {
       const result = await User.updateOne(
-        { email: user.email },          // filter
-        {role: user.role},
-        { $set: user },                 // update fields (including role)
-        { upsert: true }                // create if doesn't exist
+        { email: user.email },   // filter
+        { $set: user },          // update fields
+        { upsert: true }         // create if not exist
       );
-      console.log(`User ${user.email} seeded/updated`, result);
-      console.log(`${user.role}`);
+      console.log(`User ${user.email} seeded/updated ✅`, result);
     }
 
-    console.log("✅ All users seeded/updated");
+    console.log("🎉 All users seeded/updated");
     process.exit();
   } catch (err) {
     console.error("❌ Error seeding users:", err);
